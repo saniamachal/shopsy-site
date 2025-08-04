960c00ba03dbd1fb9a51933f7f5cda06d7309190
sania <saniamachal24@gmail.com>
2025-08-04 11:40:46 +0530

    footer update

---
+++ src/test.js
@@ -0,0 +1,18 @@
+
+  const btn = document.getElementById('mobile-menu-button');
+  const menu = document.getElementById('mobile-menu');
+
+  btn.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+  });
+
+
+       
+         
+
+
+
+       
+ 
+      
+
